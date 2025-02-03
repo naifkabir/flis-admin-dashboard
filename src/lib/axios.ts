c0977117f993228fa880336781aad6909d5d62d4
naifkabir <kabir.coder.naif@gmail.com>
2025-02-03 23:25:49 +0530

    base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:1250/api/v1";
+export const baseURL = "https://flis-backend-prod.onrender.com/api/v1";
 
 const apiClient = axios.create({
   baseURL: baseURL,
